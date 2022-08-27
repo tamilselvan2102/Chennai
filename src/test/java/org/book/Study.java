ae85e436407d63848393aab7dc16a6ca8933da14
tamilselvan2102 <tamil2102eee@gmail.com>
2022-08-27 12:19:58 +0530

    Bying

--- src/test/java/org/book/Study.java
+++ src/test/java/org/book/Study.java
@@ -1,5 +1,16 @@
 package org.book;
 
 public class Study {
+	private void method1() {
+		System.out.println();
 
+	}
+	private void method2() {
+		System.out.println();
+
+
+	}
+   private void method3() {
+	    System.out.println();
+}
 }
